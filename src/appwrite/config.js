7e56c9c41ec7b49c81d3100e791c29119aa2fa56
RAJU YADAV <rajuyadav91391@gmail.com>
2025-08-09 12:15:08 +0530

    Blog management ready to use

---
+++ src/appwrite/config.js
@@ -0,0 +1,86 @@
+import conf from "../conf/conf";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  account;
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    return await this.databases.createDocument(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId,
+      slug,
+      { title, content, featuredImage, status, userId }
+    );
+  }
+
+  async updatePost(slug, { title, content, featuedImage, status, userId }) {
+    return await this.databases.updateDocument(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId,
+      slug,
+      { title, content, featuedImage, status, userId }
+    );
+  }
+
+  async deletePost(slug) {
+    await this.databases.deleteDocument(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId,
+      slug
+    );
+    return true;
+  }
+
+  async getPost(slug) {
+    return await this.databases.getDocument(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId,
+      slug
+    );
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    if (queries) {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    }
+    return false;
+  }
+
+  async uploadFile(file) {
+    await this.account.get();
+    return await this.bucket.createFile(
+      conf.appwriteBucketId,
+      ID.unique(),
+      file
+    );
+  }
+
+  async deleteFile(fileId) {
+    await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+    return true;
+  }
+
+  getFilePreview(fileId) {
+    if (!fileId) return null;
+    return this.bucket.getFileView(conf.appwriteBucketId, fileId);
+  }
+}
+
+const service = new Service();
+export default service;
---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+import RTE from "./RTE";
+import Signup from "./SignUp";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutButton,
+  RTE,
+  Signup,
+  Login,
+  PostCard,
+  PostForm,
+  AuthLayout,
+  Button,
+  Input,
+  Select,
+  Home,
+  Post,
+};
